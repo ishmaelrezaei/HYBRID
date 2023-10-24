ef17ec80fcfc5fd4cd84c1620aed9ca886597cb1
Esmaeil <68742105+ishmaelrezaei@users.noreply.github.com>
2023-10-24 11:24:09 -0400

    Update Example_Image_Compression.py

--- Image_Compression/Example_Image_Compression.py
+++ Image_Compression/Example_Image_Compression.py
@@ -4,29 +4,31 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 sys.path.insert(0, '../RBMs_Functions/')
-from RBMs import RBD
+from RBMs_Functions import RBMs
 sys.path.pop(0)
 
-# Image compression
-file_name = 'Lena.png';
-thispic = Image.open('Images/' + file_name);
-thispic = np.array(thispic, dtype=float)
+if __name__ == "__main__":
+    # Image compression
+    file_name = 'Lena.png';
+    thispic = Image.open('Images/' + file_name);
+    thispic = np.array(thispic, dtype=float)
 
-dMax = [25, 51, 170];
-(nr, nc, nd) = thispic.shape;
-Reconstructed_image = np.zeros((nr, nc, nd), dtype=float);
-fig, axes = plt.subplots(1, 3, figsize=(16, 4))
-for d_idx, d in enumerate(dMax):
-    for i in np.arange(nd):
-        data_i = thispic[:, :, i];
-        bases, TransMat = RBD(data=data_i, tol=1e-6, col=d);
-        Reconstructed_image[:, :, i] = np.dot(bases, TransMat);
+    dMax = [25, 51, 170];
+    (nr, nc, nd) = thispic.shape;
+    Reconstructed_image = np.zeros((nr, nc, nd), dtype=float);
+    fig, axes = plt.subplots(1, 3, figsize=(16, 4))
+    for d_idx, d in enumerate(dMax):
+        for i in np.arange(nd):
+            data_i = thispic[:, :, i];
+            RB = RBMs(data=data_i, tol=1e-6, col=d)
+            bases, TransMat = RB.RBD();
+            Reconstructed_image[:, :, i] = np.dot(bases, TransMat);
 
-    # Display the reconstructed image
-    axes[d_idx].imshow(Reconstructed_image.astype(np.uint8), cmap='viridis')
-    axes[d_idx].set_title("Compressed to " + str(int(round(d/nr, 2)*100)) + "%")
-    axes[d_idx].axis('off')  # Hide the axis
-# Save the figure
-plt.savefig('ReconstructedImage.png', dpi=300, bbox_inches='tight')
-plt.show()
+        # Display the reconstructed image
+        axes[d_idx].imshow(Reconstructed_image.astype(np.uint8), cmap='viridis')
+        axes[d_idx].set_title("Compressed to " + str(int(round(d/nr, 2)*100)) + "%")
+        axes[d_idx].axis('off')  # Hide the axis
+    # Save the figure
+    plt.savefig('ReconstructedImage.png', dpi=300, bbox_inches='tight')
+    plt.show()
 
