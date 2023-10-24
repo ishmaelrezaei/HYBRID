061e8767c486ed3668382263fa15caa060d02b9f
Esmaeil <68742105+ishmaelrezaei@users.noreply.github.com>
2023-10-24 12:07:56 -0400

    Update Example_Video_Compression.py

--- Video_Compression/Example_Video_Compression.py
+++ Video_Compression/Example_Video_Compression.py
@@ -1,4 +1,16 @@
-# Image compression
+"""
+Project: HYper-reduced Basis Reduction via Interactive Decomposition
+
+@author: Esmaeil Rezaei
+link: https://github.com/ishmaelrezaei/HYBRID
+
+Note: This is a part of the HYBRID code the comprehensive code will
+be available after we publish our paper.
+
+To run this code with different examples, you can easily upload your
+video to the 'VideoSample' folder and replace 'Sample1.mp4' with the filename
+of your video."""
+
 import sys
 import cv2
 import numpy as np
@@ -7,20 +19,23 @@ from RBMs_Functions import RBMs
 sys.path.pop(0)
 
 if __name__ == "__main__":
-    # Open the video file
+    # Open the video sample file
     video_path = 'VideoSample/Sample1.mp4'  # Replace with the path to your video file
     vidcap = cv2.VideoCapture(video_path)
     # Get the total number of frames
     total_frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    dMax = 200;
-    _, frame = vidcap.read()
+    # Define a reduction level
+    dMax = 400
 
-    (nr, nc, nd) = frame.shape;
+    # Get the dimensions of the frames
+    _, frame = vidcap.read()
+    (nr, nc, nd) = frame.shape
+
     # Reset to the beginning of the video
     vidcap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-    Reconstructed_image = np.zeros((nr, nc, nd), dtype=float);
 
+    Reconstructed_frame = np.zeros((nr, nc, nd), dtype=float)
 
     # Define the video file name and codec
     video_name = 'video.avi'
@@ -31,17 +46,20 @@ if __name__ == "__main__":
     video = cv2.VideoWriter(video_name, 0, 1, (width,height))
 
     for f_idx in np.arange(total_frames):
+        # Go through each frame
         print("Reading frame " + str(f_idx+1) + " from " + str(total_frames))
         # Read the frames
-        ret, frame = vidcap.read()
+        _, frame = vidcap.read()
         frame = np.array(frame, dtype=float)
+        # Go through each dimension of the frame
         for i in np.arange(nd):
-            data_i = frame[:, :, i];
+            data_i = frame[:, :, i]
+            # Model creation and run RBD
             RB = RBMs(data=data_i, tol=1e-6, col=dMax)
-            bases, TransMat = RB.RBD();
-            Reconstructed_image[:, :, i] = np.dot(bases, TransMat);
+            bases, TransMat = RB.RBD()
+            Reconstructed_frame[:, :, i] = np.dot(bases, TransMat)
 
         # Add each image as a frame to the video
-        video.write(Reconstructed_image.astype(np.uint8))
+        video.write(Reconstructed_frame.astype(np.uint8))
     # Release the VideoWriter when done
     video.release()
