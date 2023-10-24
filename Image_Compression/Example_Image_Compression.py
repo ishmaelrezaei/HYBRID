46996de86dd912ebadafec2a8eba36adf2faa98e
Esmaeil <68742105+ishmaelrezaei@users.noreply.github.com>
2023-10-24 12:07:33 -0400

    Update Example_Image_Compression.py

--- Image_Compression/Example_Image_Compression.py
+++ Image_Compression/Example_Image_Compression.py
@@ -4,7 +4,7 @@ Project: HYper-reduced Basis Reduction via Interactive Decomposition
 @author: Esmaeil Rezaei
 link: https://github.com/ishmaelrezaei/HYBRID
 
-Note: This is a part of the HYBRID code; the comprehensive code will
+Note: This is a part of the HYBRID code the comprehensive code will
 be available after we publish our paper.
 
 To run this code with different examples, you can easily upload your
@@ -26,24 +26,24 @@ sys.path.pop(0)
 
 if __name__ == "__main__":
     # Read the data (image)
-    file_name = 'Lena.png';
-    thispic = Image.open('Images/' + file_name);
+    file_name = 'Lena.png'
+    thispic = Image.open('Images/' + file_name)
     thispic = np.array(thispic, dtype=float)
 
     # Define some levels of reduction
-    dMax = [25, 51, 170];
-    (nr, nc, nd) = thispic.shape;
-    Reconstructed_image = np.zeros((nr, nc, nd), dtype=float);
+    dMax = [25, 51, 170]
+    (nr, nc, nd) = thispic.shape
+    Reconstructed_image = np.zeros((nr, nc, nd), dtype=float)
     fig, axes = plt.subplots(1, len(dMax), figsize=(16, 4))
     # Go through each reduction level
     for d_idx, d in enumerate(dMax):
         # Go through each dimension of the image
         for i in np.arange(nd):
-            data_i = thispic[:, :, i];
+            data_i = thispic[:, :, i]
             # Model creation and run RBD
             RB = RBMs(data=data_i, tol=1e-6, col=d)
-            bases, TransMat = RB.RBD();
-            Reconstructed_image[:, :, i] = np.dot(bases, TransMat);
+            bases, TransMat = RB.RBD()
+            Reconstructed_image[:, :, i] = np.dot(bases, TransMat)
 
         # Display the reconstructed image
         axes[d_idx].imshow(Reconstructed_image.astype(np.uint8), cmap='viridis')
